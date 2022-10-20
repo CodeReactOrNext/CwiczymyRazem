13e48913a155aa5b7ca24313af112f2e93d30905
Damian Sobieraj <damiansobieraj5@gmail.com>
2022-10-20 22:14:01 +0200

    chore: Added needed page routes and removed unnecessary files

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next";
+const NotFoundPage: NextPage = () => {
+  return (
+    <div>
+      <h1>404 Page Not Found</h1>
+    </div>
+  );
+};
+
+export default NotFoundPage;
