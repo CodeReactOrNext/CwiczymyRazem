052c8a31403cc0da341bd7a37a1b50f80e36ff41
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2022-11-02 21:20:30 +0100

    feat: config redux store and add userSlice

---
+++ feature/user/store/userSlice.ts
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+};
+
+export const userSlice = createSlice({
+	name: 'user',
+	initialState,
+	reducers: {},
+});
+
+
+export const {} = userSlice.actions;
+export default userSlice.reducer;
