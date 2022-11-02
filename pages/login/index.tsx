eaecb859b833db1b62ef61f43fcfcc07e7d6422c
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2022-11-02 23:10:43 +0100

    feat: dummy login/logout logic

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,8 +1,15 @@
 import type { NextPage } from "next";
+import { selectUserAuth } from "../../feature/user/store/userSlice";
+import LoginView from "../../feature/user/view/LoginView/LoginView";
+import LogoutView from "../../feature/user/view/LogoutView/LogoutView";
+import { useAppSelector } from "../../store/hooks";
+
 const LoginPage: NextPage = () => {
+  const isUserLogin = useAppSelector(selectUserAuth);
   return (
     <div>
       <h1>LoginPage</h1>
+      {isUserLogin ? <LogoutView /> : <LoginView />}
     </div>
   );
 };
