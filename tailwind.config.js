72d427ee65ae5c98f1653336bc9eec377dc9ea51
Damian Sobieraj <damiansobieraj5@gmail.com>
2022-11-10 18:32:33 +0100

    chore: Added Tailwind theme colors and font, added Head component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "main-red": "#FF2A37",
+        "light-blue": "#1193A4",
+        "dark-blue": "#262E52",
+        "light-beige": "#F5D898",
+      },
+    },
+    fontFamily: {
+      sans: ["Teko"],
+    },
   },
   plugins: [],
 };
