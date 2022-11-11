8dbcade743a84b0af509fd76f46586ce38ae0532
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2022-11-11 11:37:31 +0100

    feat: add button component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from "next";
+import Button from "../components/Button";
 
 const Home: NextPage = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <Button>Ćwiczymy Razem!</Button>
     </div>
   );
 };
