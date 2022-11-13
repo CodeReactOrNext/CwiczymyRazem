e1a32981e0ae0d4f4336873933f4b718ede0a243
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2022-11-13 10:12:24 +0100

    feat: add input component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from "next";
 import MainLayout from "../layouts/MainLayout";
 import Button from "../components/Button";
+import Input from "../components/Input";
+import { FaUserAlt } from "react-icons/fa";
 
 const Home: NextPage = () => {
   return (
@@ -9,6 +11,7 @@ const Home: NextPage = () => {
       subtitle='Ćwicz, raportuj, zdobywaj punkty!'>
       <h1>Home Page</h1>
       <Button>Ćwiczymy Razem!</Button>
+      <Input Icon={FaUserAlt} />
     </MainLayout>
   );
 };
