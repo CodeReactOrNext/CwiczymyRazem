6ef3428cab6ecaff0df36576972c3f1f9b503c4a
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2022-11-13 10:12:24 +0100

    feat: add input component

---
+++ components/Input/Input.tsx
@@ -0,0 +1,25 @@
+import { IconType } from "react-icons/lib";
+
+export interface InputProps {
+  Icon?: IconType;
+  placeholder?: string;
+}
+
+const Input = ({ Icon, placeholder }: InputProps) => {
+  return (
+    <div className='flex flex-row items-center justify-center'>
+      {Icon && (
+        <div className='flex h-[60px] w-[60px] items-center justify-center bg-main-opposed'>
+          <Icon size='24' />
+        </div>
+      )}
+      <input
+        className='bg-tertiary p-2 text-xl text-main-opposed focus:outline-none focus:ring focus:ring-main-opposed'
+        type='text'
+        placeholder={placeholder}
+      />
+    </div>
+  );
+};
+
+export default Input;
