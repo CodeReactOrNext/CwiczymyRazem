dc14fa909615b0a8ff8d60c99d348d2adc855a2f
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2022-11-13 21:31:30 +0100

    feat: separated Slogan from LoginView

---
+++ components/Slogan/Slogan.tsx
@@ -0,0 +1,12 @@
+const Slogan = () => {
+  return (
+    <div className='w-full'>
+      <p className='text-8xl font-medium uppercase text-tertiary '>Ćwiczymy</p>
+      <p className='relative bottom-[50px]	left-[260px] text-7xl font-bold uppercase text-main-opposed'>
+        Razem
+      </p>
+    </div>
+  );
+};
+
+export default Slogan;
