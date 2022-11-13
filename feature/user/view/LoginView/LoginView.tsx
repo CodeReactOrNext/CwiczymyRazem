897cd76a29aa51216964e14cb124429d660f4f57
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2022-11-13 21:31:30 +0100

    feat: separated Slogan from LoginView

--- feature/user/view/LoginView/LoginView.tsx
+++ feature/user/view/LoginView/LoginView.tsx
@@ -9,6 +9,7 @@ import {
 import { addUserAuth, addUserName } from "../../store/userSlice";
 import { FaUserAlt, FaLock } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
+import Slogan from "../../../../components/Slogan";
 
 const LoginView = () => {
   const dispatch = useDispatch();
@@ -21,23 +22,17 @@ const LoginView = () => {
 
   return (
     <MainLayout subtitle='Ćwicz, raportuj, zdobywaj punkty!' variant='primary'>
-      <div className=' flex	 flex-col items-center  justify-center space-y-4'>
-        <div className='w-[420px]'>
-          <p className='text-8xl font-medium uppercase text-tertiary '>
-            Ćwiczymy
-          </p>
-          <p className='relative bottom-[50px]	left-[260px] text-7xl font-bold uppercase text-main-opposed'>
-            Razem
-          </p>
-        </div>
-
+      <div className=' flex flex-col items-center  justify-center space-y-4 '>
+        <Slogan />
         <Input Icon={FaUserAlt} placeholder={"Login"} />
         <Input Icon={FaLock} placeholder={"Hasło"} />
         <div className='flex space-x-1 '>
-          <Button onClick={logGoogleUser}>Loguj</Button>
+          <Button>Loguj</Button>
           <Button variant='secondary'>Rejestracja</Button>
         </div>
-        <button className='flex flex-row gap-2  bg-white p-3 text-black'>
+        <button
+          onClick={logGoogleUser}
+          className='flex flex-row gap-3 bg-white p-3 font-medium tracking-wide text-[#555555]'>
           <FcGoogle size='24' /> Zaloguj się z Google
         </button>
       </div>
