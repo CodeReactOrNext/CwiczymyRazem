406e365138d5f09b978091f98eb71f8cb5298e1e
Damian Sobieraj <damiansobieraj5@gmail.com>
2022-11-15 10:53:00 +0100

    feat: added responsive navigation background and linked Hero button with login page

--- layouts/MainLayout/components/Navigation.tsx
+++ layouts/MainLayout/components/Navigation.tsx
@@ -1,10 +1,19 @@
 import Link from "next/link";
 import { FaBars } from "react-icons/fa";
 
-export default function Navigation() {
+export default function Navigation({
+  variant,
+}: {
+  variant: "primary" | "secondary" | "landing";
+}) {
   return (
-    <nav className='right-0 top-0 z-50'>
-      <ul className='hidden gap-8 text-3xl lg:flex 2xl:gap-32'>
+    <nav
+      className={`clip-nav right-0 top-0 z-50 flex h-full items-center justify-center pr-8 lg:w-2/5 ${
+        variant === "landing" ? "xl:w-1/3" : ""
+      }  ${
+        variant === "secondary" ? "lg:bg-main-opposed-500" : "lg:bg-second-500"
+      }  lg:pl-16`}>
+      <ul className='hidden w-full justify-evenly gap-8 text-3xl lg:flex'>
         <li>
           <Link href='/leaderboard'>Leaderboard</Link>
         </li>
@@ -15,13 +24,11 @@ export default function Navigation() {
           <Link href='/faq'>FAQ</Link>
         </li>
       </ul>
-      {/* PLACEHOLDER */}
       <button className='lg:hidden'>
         <ul className='flex flex-col gap-1'>
           <FaBars />
         </ul>
       </button>
-      {/* PLACEHOLDER */}
     </nav>
   );
 }
