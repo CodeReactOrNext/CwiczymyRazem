074ef00feebc35bb501f1505ef418757afc0d024
Damian Sobieraj <damiansobieraj5@gmail.com>
2022-11-15 10:17:22 +0100

    fix: fixed content positioning in MainLayout and added pointer events none to make Lightning click-trough

--- layouts/HeroLayout/HeroLayout.tsx
+++ layouts/HeroLayout/HeroLayout.tsx
@@ -40,6 +40,8 @@ export default function HeroLayout({
           </p>
         </span>
       </div>
+      <div
+        className={`absolute h-full max-h-[1080px] w-full max-w-[1920px] bg-old-effect bg-cover bg-no-repeat lg:bg-old-effect-hr`}></div>
       <div className='z-10 flex flex-col items-center justify-center gap-6 xsm:flex-row lg:-mb-24 lg:flex-col xl:pr-8'>
         <span className='text-left text-[2.5vh] text-tertiary-500 xxs:text-[2.7vh] xsm:text-[2vh] md:text-2xl lg:text-right xl:text-3xl 2xl:text-4xl'>
           {children}
