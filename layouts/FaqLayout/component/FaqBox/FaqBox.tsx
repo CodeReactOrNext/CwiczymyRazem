d9d1500558b816bd90503b126ed9bda1c42697b4
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2022-11-20 15:44:45 +0100

    feat: faq

---
+++ layouts/FaqLayout/component/FaqBox/FaqBox.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { FaMinus, FaPlus } from "react-icons/fa";
+
+interface FaqBoxProps {
+  opened?: boolean;
+  title: string;
+  message: string;
+}
+
+const FaqBox = ({ opened, title, message }: FaqBoxProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className='cursor-pointer' onClick={() => setIsOpen(!isOpen)}>
+      <div className='flex  h-20 w-full flex-row items-center gap-4 bg-second p-4 text-4xl text-tertiary hover:bg-second-100'>
+        <div className='text-main-opposed'>
+          {isOpen ? <FaMinus /> : <FaPlus />}
+        </div>
+        <p>{title}</p>
+      </div>
+      {isOpen && (
+        <div className='mt-1 flex w-full flex-row items-center bg-second p-4 text-xl text-tertiary'>
+          <p> {message}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FaqBox;
