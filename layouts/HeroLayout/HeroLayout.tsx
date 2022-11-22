462418cfa841867866283902b075a00773cb70c1
Damian Sobieraj <damiansobieraj5@gmail.com>
2022-11-22 16:35:02 +0100

    feat: added thunder shake animation

--- layouts/HeroLayout/HeroLayout.tsx
+++ layouts/HeroLayout/HeroLayout.tsx
@@ -20,26 +20,34 @@ export default function HeroLayout({
           <div className='relative h-full w-full'>
             <FireDoubleSVG className='absolute top-[60%] left-0 right-0 w-[110%] -translate-x-[7%] fill-second-500 sm:top-[50%] md:top-[50%] lg:-bottom-[45%] lg:top-auto lg:h-full xl:-bottom-[50%]' />
             <LightningSVG className='absolute top-[20%] right-[15%] w-[20vw] fill-tertiary-500 sm:-top-[10%] md:right-[10%] lg:right-[10%] lg:top-[12%] lg:w-[30%] xl:top-[10%]' />
-            <LightningSVG className='absolute bottom-0 right-[20%] w-[10vw] rotate-[110deg] fill-tertiary-500 sm:right-[35%] sm:w-[5vw] lg:right-[30%] lg:w-[15%]' />
-            <LightningRevSVG className='absolute bottom-[5%] left-[15%] w-[20vw] rotate-[140deg] fill-tertiary-500 sm:left-[30%] sm:w-[15vw] lg:left-[15%] lg:w-[20%]' />
-          </div>
-          <div className='absolute left-[50%]  bottom-0 z-40 h-fit w-[25%] max-w-[100px] -translate-x-[50%] lg:bottom-[15%] lg:left-[47%] lg:max-w-max xl:left-[43%]'>
-            <div className='shadow-animation absolute -bottom-[15%] aspect-square w-full rounded-full'></div>
-            <img
-              className='hover-animation h-full w-full'
-              src='/static/images/guitar_red.png'
-              alt='red guitar'
-            />
+            {/* <LightningSVG className='absolute bottom-0 right-[20%] w-[10vw] rotate-[110deg] fill-tertiary-500 sm:right-[35%] sm:w-[5vw] lg:right-[30%] lg:w-[15%]' /> */}
+            {/* <LightningRevSVG className='absolute bottom-[5%] left-[15%] w-[20vw] rotate-[140deg] fill-tertiary-500 sm:left-[30%] sm:w-[15vw] lg:left-[15%] lg:w-[20%]' /> */}
           </div>
         </div>
-        <span className='relative z-50 flex h-[50%] flex-col pt-[8%] text-[20vw] font-medium leading-[0.7] sm:text-[14vw] lg:text-[8em]'>
+        {/* <span className='relative z-50 flex h-[50%] flex-col pt-[8%] text-[20vw] font-medium leading-[0.7] sm:text-[14vw] lg:text-[8em]'>
           <p className='self-start pl-[10%] text-left text-tertiary-500 shadow-md'>
             ĆWICZYMY
           </p>
           <p className='self-end pr-[10%] text-[0.8em] font-bold text-second-500 '>
             RAZEM
           </p>
-        </span>
+        </span> */}
+        <div className='h-full w-full'>
+          <div className='absolute left-[50%] bottom-[5%] z-40 h-fit w-[17%] max-w-[100px] -translate-x-[50%] lg:bottom-[15%] lg:left-[47%] lg:w-[25%] lg:max-w-max xl:left-[43%]'>
+            <div className='shadow-animation absolute -bottom-[15%] aspect-square w-full rounded-full'></div>
+            <img
+              className='hover-animation h-full w-full'
+              src='/static/images/guitar_red.png'
+              alt='red guitar'
+            />
+            <div className='shake-animation absolute -bottom-[5%] -left-[100%] w-[65%] lg:-bottom-[15%]'>
+              <LightningRevSVG className='origin-right rotate-[140deg] fill-tertiary-500 ' />
+            </div>
+            <div className='shake-animation absolute bottom-[5%] -right-[70%] w-[40%] lg:-bottom-[10%]'>
+              <LightningSVG className='origin-left rotate-[110deg] fill-tertiary-500' />
+            </div>
+          </div>
+        </div>
       </div>
       <div className='z-10 flex flex-col items-center justify-center gap-6 xsm:flex-row lg:-mb-24 lg:flex-col xl:pr-8'>
         <span className='text-left text-[2.5vh] text-tertiary-500 xxs:text-[2.7vh] xsm:text-[2vh] md:text-2xl lg:text-right xl:text-3xl 2xl:text-4xl'>
@@ -51,7 +59,7 @@ export default function HeroLayout({
           </a>
         </Link>
       </div>
-      <OldEffect />
+      {/* <OldEffect /> */}
     </div>
   );
 }
