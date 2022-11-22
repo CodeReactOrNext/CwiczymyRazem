bf1dee6561fa65a65bf83706b1eb55909f275bc6
Damian Sobieraj <damiansobieraj5@gmail.com>
2022-11-22 15:44:38 +0100

    refactor: refactored Layouts

--- layouts/MainLayout/components/ContentBox.tsx
+++ layouts/MainLayout/components/ContentBox.tsx
@@ -8,11 +8,11 @@ interface Props {
 export default function ContentBox({ children, variant }: Props) {
   return (
     <section
-      className={`relative z-40 flex h-full w-full items-center justify-center py-8 2xl:w-full 2xl:justify-center ${
+      className={`relative z-40 flex h-full w-full items-center justify-center overflow-y-auto py-8 2xl:w-full  2xl:justify-center ${
         variant === "landing" ? "z-0" : "lg:w-4/6 xl:w-5/6"
       } `}>
       <div
-        className={`absolute top-0 bottom-0 left-0 right-0 m-auto flex h-fit justify-center overflow-y-auto overflow-x-hidden ${
+        className={`absolute top-0 bottom-0 left-0 right-0 my-auto flex justify-center overflow-x-hidden ${
           variant === "landing" ? "h-[100%] overflow-y-hidden" : ""
         }`}>
         {children}
--- layouts/MainLayout/components/SubtitleBar.tsx
+++ layouts/MainLayout/components/SubtitleBar.tsx
@@ -7,18 +7,18 @@ interface Props {
 export default function SubtitleBar({ variant, children }: Props) {
   return (
     <div
-      className={`h20 relative z-50 flex w-[120%] -translate-x-[10%] items-center justify-end lg:h-20
+      className={`z-50 flex h-auto w-[120%] -translate-x-[10%] items-center justify-end lg:h-20
     ${
       variant === "landing"
         ? "absolute top-[50%] -translate-y-[50%] lg:relative lg:top-0 lg:translate-y-0"
-        : ""
+        : "relative"
     }`}>
       <div
         className={`clip-bg-mobile absolute -z-10 h-full w-full  translate-y-[1px]  lg:hidden ${
           variant === "secondary" ? "bg-main-opposed-500" : "bg-second-500"
         } ${variant === "landing" ? "bg-transparent" : ""}`}></div>
       <LightningRevSVG
-        className={`min-h-full w-full fill-tertiary-500 lg:hidden ${
+        className={`min-h-full w-full fill-tertiary-500 rotate-x-40 lg:hidden ${
           variant === "landing" ? "" : ""
         }`}
       />
