eb1632789381512391a5de1d914a56f93eecafc7
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2022-11-23 23:01:20 +0100

    feat: leadboard mobile

--- layouts/LeadboardLayout/LeadboardLayout.tsx
+++ layouts/LeadboardLayout/LeadboardLayout.tsx
@@ -4,14 +4,24 @@ import LeadboardColumn from "./components/LeadboardColumn/LeadboardColumn";
 const LeadboardLayout = () => {
   return (
     <MainLayout subtitle='Leadboard' variant='secondary'>
-      <div className='w-[80%]'>
-        <div className='flex w-full justify-end gap-6 text-xl'>
+      <div>
+        <div className='flex w-full justify-end gap-6 px-4 text-xl'>
           <p>Dzień</p>
           <p>Tydzień</p>
           <p>Wszystko</p>
         </div>
 
         <LeadboardColumn place={1} />
+
+        <LeadboardColumn place={2} />
+        <LeadboardColumn place={3} />
+        <LeadboardColumn place={4} />
+        <LeadboardColumn place={2} />
+        <LeadboardColumn place={3} />
+        <LeadboardColumn place={4} />
+        <LeadboardColumn place={2} />
+        <LeadboardColumn place={3} />
+        <LeadboardColumn place={4} />
         <LeadboardColumn place={2} />
         <LeadboardColumn place={3} />
         <LeadboardColumn place={4} />
