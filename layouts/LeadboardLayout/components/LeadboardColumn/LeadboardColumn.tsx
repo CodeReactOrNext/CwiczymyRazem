4b134239950a351f3737fb2d02148d6bbd888086
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2022-11-23 23:01:20 +0100

    feat: leadboard mobile

--- layouts/LeadboardLayout/components/LeadboardColumn/LeadboardColumn.tsx
+++ layouts/LeadboardLayout/components/LeadboardColumn/LeadboardColumn.tsx
@@ -8,33 +8,42 @@ interface LeadboardColumnProps {
 const LeadboardColumn = ({ place }: LeadboardColumnProps) => {
   return (
     <div className='flex w-full  p-5'>
-      <p className='flex  w-[100px] items-end justify-end text-6xl font-semibold text-tertiary'>
+      <p className='     flex items-center justify-end text-4xl font-semibold  text-tertiary md:text-4xl lg:w-[100px] lg:text-6xl'>
         {place + "."}
       </p>
-      <div className='ml-5 flex h-16 w-full max-w-[800px] items-center '>
-        <Avatar />
-        <div className=' mr-5 flex h-16 w-full items-center justify-between bg-second bg-opacity-75 px-2'>
-          <div className='relative'>
-            <p className='text-2xl'>Krokon</p>
-            <div className='absolute top-[-35px] right-[-60px] flex items-center gap-x-1'>
+      <div className=' ml-2 flex w-full max-w-[800px] items-center md:ml-5 md:h-16 '>
+        <div className='hidden md:block'>
+          <Avatar />
+        </div>
+        <div className=' mr-5 grid w-full grid-cols-3 grid-rows-3 justify-items-center  bg-second bg-opacity-75 px-2 md:h-16 md:grid-rows-1'>
+          <div className='relative top-[-15px] left-[-25px] block h-[65px] scale-75 justify-items-start md:hidden'>
+            <Avatar />
+            <div className='absolute top-[5px] right-[-60px] flex  items-center gap-x-1 '>
               <p className='text-xl uppercase text-tertiary'>Lvl </p>
-              <p className='text-5xl  text-main'>34</p>
+              <p className='text-3xl text-main '>34</p>
             </div>
           </div>
-          <div className='flex h-full w-[300px] justify-center gap-x-5'>
-            <div className='flex flex-col items-center justify-end px-2 '>
+          <div className='relative col-span-2 self-center justify-self-start md:col-span-1 '>
+            <p className='text-2xl'>Krokon</p>
+            <div className='absolute top-[-20px] right-[-60px]  hidden items-center  gap-x-1 md:top-[-35px] md:flex'>
+              <p className='text-xl uppercase text-tertiary'>Lvl </p>
+              <p className='text-4xl text-main  md:text-5xl'>34</p>
+            </div>
+          </div>
+          <div className='col-span-3 flex h-full w-full items-center justify-evenly md:col-span-1 md:w-[300px] md:justify-center  md:gap-x-5'>
+            <div className='flex  flex-col items-center md:justify-end md:px-2 '>
               <p className='text-3xl leading-[22px] '>12415</p>
               <p className='leading-[25px] text-tertiary'>Punktów</p>
             </div>
-            <div className='flex  flex-col items-center justify-end px-2'>
+            <div className='flex  flex-col items-center md:justify-end md:px-2'>
               <p className='text-3xl leading-[22px]'>14:15</p>
               <p className='  leading-[25px] text-tertiary'>Czas Ćwiczeń</p>
             </div>
           </div>
-          <div className='flex h-full  flex-col items-center justify-end'>
+          <div className=' col-span-3 flex h-full w-full flex-col items-center justify-center  md:col-span-1  md:w-fit md:justify-end '>
             <div className='flex text-2xl'>
               <FaAngleLeft className='cursor-pointer text-main-opposed hover:text-white' />
-              <div className='flex w-[150px] justify-around text-xl '>
+              <div className='flex w-[100px] justify-around text-xl xs:w-[150px] '>
                 <FaEvernote />
                 <FaEvernote />
                 <FaEvernote />
