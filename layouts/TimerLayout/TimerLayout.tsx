473a89a7913051cc4abbb6750f3f784a834ddb59
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2022-11-23 23:57:15 +0100

    feat: time and exceries paragraph

--- layouts/TimerLayout/TimerLayout.tsx
+++ layouts/TimerLayout/TimerLayout.tsx
@@ -10,9 +10,18 @@ const TimerLayout = () => {
         <p className=' text-8xl tracking-wider text-tertiary sm:text-9xl'>
           02:23
         </p>
+
         <div className='mb-14 flex flex-row gap-x-5 bg-second p-3 px-7 text-tertiary'>
           <FaPlay size={40} />
           <FaPause size={40} />
+        </div>
+        <div className='flex flex-row gap-5 text-2xl'>
+          <p>
+            Czas łącznie: <span className='text-tertiary'>4:50</span>
+          </p>
+          <p>
+            Aktualnie ćwiczysz: <span className='text-tertiary'>Technika</span>
+          </p>
         </div>
         <div className='mb-14  flex w-[330px] flex-row flex-wrap justify-center md:w-[570px] lg:w-full   '>
           <CategoryBox title='Technika' chosen />
