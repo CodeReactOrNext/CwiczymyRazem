c8481914144b911346a04c7cb7e927ee00065070
Damian Sobieraj <damiansobieraj5@gmail.com>
2022-11-25 15:31:22 +0100

    feat: thinned down the LightningDivider to make more space for content

--- layouts/MainLayout/components/ContentBox.tsx
+++ layouts/MainLayout/components/ContentBox.tsx
@@ -9,7 +9,7 @@ export default function ContentBox({ children, variant }: Props) {
   return (
     <section
       className={`relative z-40 flex h-full w-full items-center justify-center overflow-y-auto py-8 2xl:w-full 2xl:justify-center ${
-        variant === "landing" ? "z-0" : "lg:w-4/6 xl:w-5/6"
+        variant === "landing" ? "z-0" : "lg:w-full"
       } `}>
       <div
         className={`absolute top-0 bottom-0 left-0 right-0 my-auto flex justify-center overflow-x-hidden py-8 ${
