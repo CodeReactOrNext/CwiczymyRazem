948c17818230f4b371391dc2c49ff92c1f50fcf5
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2022-11-28 21:57:50 +0100

    refactor: change name of SettingView and add link to settings

---
+++ pages/settings/index.tsx
@@ -0,0 +1,8 @@
+import SettingView from "feature/user/view/SettingView";
+import type { NextPage } from "next";
+
+const Settings: NextPage = () => {
+  return <SettingView />;
+};
+
+export default Settings;
