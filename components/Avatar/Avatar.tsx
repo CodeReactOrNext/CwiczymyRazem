0ab0c1abe138c905d34bed8fe0d6a972d9d7d749
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2022-11-30 19:04:41 +0100

    feat: default avatar

--- components/Avatar/Avatar.tsx
+++ components/Avatar/Avatar.tsx
@@ -1,7 +1,15 @@
-const Avatar = () => {
+interface AvatarProps {
+  user: {
+    name: string;
+    avatar?: string;
+  };
+}
+const Avatar = ({ user }: AvatarProps) => {
   return (
     <div className='relative'>
-      <div className='h-20  w-20 bg-white'></div>
+      <div className='flex  h-20 w-20 items-center justify-center bg-tertiary-400'>
+        <p className='text-5xl uppercase text-main-opposed'>{user.name[0]}</p>
+      </div>
       <img
         className='absolute bottom-[18px] left-[35px] -rotate-90'
         src='/static/images/rank/3.png'
