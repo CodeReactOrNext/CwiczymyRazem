b32ee6af07bb2f8ddc8480d183b685f7398d5d16
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2022-12-03 14:58:05 +0100

    feat: new design of ReportFormLayout

---
+++ layouts/ReportFormLayout/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Checkbox } from "./Checkbox";
+export { default as InputTime } from "./InputTime";
+export { default as ReportCategoryWrapper } from "./ReportCategoryWrapper";
+export { default as TimeInputBox } from "./TimeInpuBox";
