d490372f47115b0b812ee1d17f4c7159f26e4622
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2022-12-05 19:07:29 +0100

    feat: google login and logout handling

--- feature/user/store/userSlice.ts
+++ feature/user/store/userSlice.ts
@@ -1,10 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { User } from "firebase/auth";
+import Router from "next/router";
+import {
+  createUserDocumentFromAuth,
+  getUserData,
+  signInWithGooglePopup,
+} from "utils/firebase/firebase.utils";
+import { statisticsDataInterface } from "utils/firebase/userStatisticsInitialData";
 import { RootState } from "../../../store/store";
 
-const initialState = {
+const initialState: {
+  userAuth: string | null;
+  userInfo: User | null;
+  userData: statisticsDataInterface | null;
+} = {
+  userInfo: null,
   userAuth: null,
   userData: null,
 };
+
+export const logInViaGoogle = createAsyncThunk(
+  "user/logInViaGoogle",
+  async (parameters, thunkAPI) => {
+    const { user } = await signInWithGooglePopup();
+    const userAuth = await createUserDocumentFromAuth(user);
+    const userData = await getUserData(userAuth);
+    return { user, userAuth, userData };
+  }
+);
 
 export const userSlice = createSlice({
   name: "user",
@@ -19,6 +42,22 @@ export const userSlice = createSlice({
     addUserData: (state, action) => {
       state.userData = action.payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(logInViaGoogle.pending, (state) => {
+        //Loadgin Screen
+      })
+      .addCase(logInViaGoogle.fulfilled, (state, action) => {
+        state.userInfo = action.payload.user;
+        state.userData = action.payload.userData;
+        state.userAuth = action.payload.userAuth;
+        Router.push("/");
+      })
+      .addCase(logInViaGoogle.rejected, (state, action) => {
+        //errorr handling
+        console.log(action.payload);
+      });
   },
 });
 
