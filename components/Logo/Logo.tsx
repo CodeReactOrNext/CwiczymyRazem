1b4a74952ddb95de4de27a41285be9dc86c7d211
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2022-12-05 23:31:36 +0100

    feat: add i18n to logo

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
+import { useTranslation } from "react-i18next";
 
 const Logo = () => {
-  
+  const { t } = useTranslation("landing");
   return (
     <Link href='/'>
       <div className='z-50 flex cursor-pointer'>
-        <div className='flex flex-col items-end justify-center p-2 leading-4 '>
-          <p className='font-bold  text-mainText '>Ćwiczymy</p>
-          <p className='font-bold text-tertiary '>Razem</p>
+        <div className='flex flex-col items-end justify-center p-2 leading-4'>
+          <p className='font-bold  text-mainText'>{t("slogan_line_1")}</p>
+          <p className='font-bold text-tertiary'>{t("slogan_line_2")}</p>
         </div>
       </div>
     </Link>
