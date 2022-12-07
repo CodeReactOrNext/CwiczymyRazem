79ccefe86a808d36e070230294a4f4cc0f96fba0
Damian Sobieraj <damiansobieraj5@gmail.com>
2022-12-07 22:01:22 +0100

    feat: created Yup schemas and linked login and signup forms with Formik

--- feature/user/view/LoginView/LoginView.tsx
+++ feature/user/view/LoginView/LoginView.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import Router from "next/router";
+import { Formik, Form } from "formik";
+import { loginSchema } from "schemas/login";
 import { useTranslation } from "react-i18next";
 import { logInViaGoogle } from "../../store/userSlice";
 import { useAppDispatch } from "store/hooks";
@@ -10,9 +12,6 @@ import Button from "components/Button";
 import GoogleButton from "components/GoogleButton";
 import Input from "components/Input";
 
-import { Formik, Field, Form, ErrorMessage } from "formik";
-import * as Yup from "yup";
-
 const LoginView = () => {
   const { t } = useTranslation(["common", "login"]);
   const dispatch = useAppDispatch();
@@ -21,27 +20,52 @@ const LoginView = () => {
     dispatch(logInViaGoogle());
   };
 
+  function onSubmit() {
+    console.log("submitted");
+  }
+
+  const formikInitialValues = {
+    email: "",
+    password: "",
+  };
+
   return (
     <MainLayout subtitle={t("login:subtitlebar_text")} variant='primary'>
-      <FormLayout>
-        <>
-          <Input Icon={FaUserAlt} placeholder={t("common:input.login")} />
-          <Input Icon={FaLock} placeholder={t("common:input.password")} />
-          <div className='flex space-x-1 '>
-            <Button>{t("common:button.sign_in")}</Button>
-            <Link href='/signup'>
-              <a>
-                <Button variant='secondary'>
-                  {t("common:button.sign_up")}
-                </Button>
-              </a>
-            </Link>
-          </div>
-          <GoogleButton onClick={googleLogInHandler}>
-            {t("common:google_button.sign_in")}
-          </GoogleButton>
-        </>
-      </FormLayout>
+      <Formik
+        initialValues={formikInitialValues}
+        validationSchema={loginSchema}
+        onSubmit={onSubmit}>
+        <Form>
+          <FormLayout>
+            <>
+              <Input
+                name='email'
+                Icon={FaUserAlt}
+                placeholder={t("common:input.login")}
+              />
+              <Input
+                name='password'
+                type='password'
+                Icon={FaLock}
+                placeholder={t("common:input.password")}
+              />
+              <div className='flex space-x-1 '>
+                <Button>{t("common:button.sign_in")}</Button>
+                <Link href='/signup'>
+                  <a>
+                    <Button variant='secondary'>
+                      {t("common:button.sign_up")}
+                    </Button>
+                  </a>
+                </Link>
+              </div>
+              <GoogleButton onClick={googleLogInHandler}>
+                {t("common:google_button.sign_in")}
+              </GoogleButton>
+            </>
+          </FormLayout>
+        </Form>
+      </Formik>
     </MainLayout>
   );
 };
---
+++ schemas/passwordRules.ts
@@ -0,0 +1,3 @@
+export const passwordRegexRules =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+// "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
