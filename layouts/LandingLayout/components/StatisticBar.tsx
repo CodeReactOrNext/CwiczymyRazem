c17ccc49a62d5f2131ff2f15836a1044c81fa916
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2022-12-12 00:07:48 +0100

    fix: Nan in percentValue in StatisticBar

--- layouts/LandingLayout/components/StatisticBar.tsx
+++ layouts/LandingLayout/components/StatisticBar.tsx
@@ -8,13 +8,15 @@ export default function StatisticBar({
   value,
   percent,
 }: StatisticBarProps) {
+  const percentValue = percent ? percent : 0;
+
   return (
     <div className='z-10 flex flex-col items-center sm:text-xl '>
-      <p className='text-sm sm:text-lg'>{percent}%</p>
+      <p className='text-sm sm:text-lg'>{percentValue}%</p>
       <div className='flex h-32 w-5 bg-main-opposed-50 sm:h-40 sm:w-8'>
         <div
           className='w-5 self-end bg-main sm:w-8'
-          style={{ height: percent + "%" }}
+          style={{ height: percentValue + "%" }}
         />
       </div>
       <p className='my-2 w-full bg-main-opposed px-5 sm:px-10 md:px-5 lg:px-10'>
