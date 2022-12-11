6763375d5fa0262abac6b86cc9d0576c58a0205e
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2022-12-11 21:33:35 +0100

    feat: loading spinner on googleButton

--- components/GoogleButton/GoogleButton.tsx
+++ components/GoogleButton/GoogleButton.tsx
@@ -1,4 +1,8 @@
+import { selectIsFetching } from "feature/user/store/userSlice";
+import { is } from "immer/dist/internal";
 import { FcGoogle } from "react-icons/fc";
+import { CircleSpinner } from "react-spinners-kit";
+import { useAppSelector } from "store/hooks";
 
 interface GoogleButtonProps {
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
@@ -6,11 +10,17 @@ interface GoogleButtonProps {
 }
 
 const GoogleButton = ({ onClick, children }: GoogleButtonProps) => {
+  const isFetching = useAppSelector(selectIsFetching);
   return (
     <button
       onClick={onClick}
-      className='flex flex-row gap-3 bg-white p-3 font-medium tracking-wide text-[#555555]'>
-      <FcGoogle size='24' />
+      className='flex flex-row gap-3 bg-white p-3 font-medium tracking-wide text-[#555555]'
+      disabled={isFetching ? true : false}>
+      {isFetching ? (
+        <CircleSpinner size='24' color='#555555' />
+      ) : (
+        <FcGoogle size='24' />
+      )}
       {children}
     </button>
   );
