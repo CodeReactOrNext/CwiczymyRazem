7e5057a2fb797d248c26c6cd1851b91defed270f
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2022-12-11 23:58:48 +0100

    feat: dynamic StatisticBar

--- layouts/LandingLayout/components/StatisticBar.tsx
+++ layouts/LandingLayout/components/StatisticBar.tsx
@@ -1,13 +1,21 @@
 interface StatisticBarProps {
   title: string;
-  value: number;
+  value: string;
+  percent: number;
 }
-export default function StatisticBar({ title, value }: StatisticBarProps) {
+export default function StatisticBar({
+  title,
+  value,
+  percent,
+}: StatisticBarProps) {
   return (
     <div className='z-10 flex flex-col items-center sm:text-xl '>
-      <p className='text-sm sm:text-lg'>76%</p>
+      <p className='text-sm sm:text-lg'>{percent}%</p>
       <div className='flex h-32 w-5 bg-main-opposed-50 sm:h-40 sm:w-8'>
-        <div className=' h-[20%] w-5 self-end bg-main sm:w-8'></div>
+        <div
+          className='w-5 self-end bg-main sm:w-8'
+          style={{ height: percent + "%" }}
+        />
       </div>
       <p className='my-2 w-full bg-main-opposed px-5 sm:px-10 md:px-5 lg:px-10'>
         {value}h
