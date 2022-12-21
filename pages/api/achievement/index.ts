b0e803eabfe001eb9d0e46a526d5321818d085ed
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2022-12-21 20:29:11 +0100

    feat: backend achievements check

---
+++ pages/api/achievement/index.ts
@@ -0,0 +1,46 @@
+import {
+  ReportDataInterface,
+  ReportFormikInterface,
+} from "feature/user/view/ReportView/ReportView.types";
+import { StatisticsDataInterface } from "utils/firebase/userStatisticsInitialData";
+import type { NextApiRequest, NextApiResponse } from "next";
+import {
+  checkBalance,
+  checkFire,
+  checkHealthHabits,
+  time1Check,
+  time2Check,
+  time3Check,
+} from "./utils/achievementMethods";
+
+export const checkAchievement = (
+  statistics: StatisticsDataInterface,
+  reportData: ReportDataInterface,
+  inputData: ReportFormikInterface
+) => {
+  const achievedAchievements = [
+    time1Check(statistics),
+    time2Check(statistics),
+    time3Check(statistics),
+    checkBalance(inputData),
+    checkFire(reportData),
+    checkHealthHabits(reportData),
+  ];
+
+  const isAchievements = (item: string | undefined): item is string => {
+    return !!item;
+  };
+
+  const userAchievedAchievements = achievedAchievements
+    .filter(isAchievements)
+    .filter((item) => !statistics.achievements.includes(item));
+
+  return userAchievedAchievements;
+};
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { statistics, raiting, inputData } = JSON.parse(req.body);
+  const achievement = checkAchievement(statistics, raiting, inputData);
+
+  res.status(200).json(JSON.stringify(achievement));
+}
