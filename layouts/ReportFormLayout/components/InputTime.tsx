d1d4162c50ceb3c0a7a4e2c7260b9e09c4a8acbe
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2022-12-22 01:26:21 +0100

    feat: clickable time input button

--- layouts/ReportFormLayout/components/InputTime.tsx
+++ layouts/ReportFormLayout/components/InputTime.tsx
@@ -3,20 +3,26 @@ import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 import { toast } from "react-toastify";
 
 export default function InputTime({ name }: { name: string }) {
-  const [field, meta] = useField(name);
+  const [field, meta, helpers] = useField(name);
+
   return (
     <>
       <div className='flex max-w-[3rem] flex-col items-center gap-y-2'>
-        <FaChevronUp />
+        <FaChevronUp
+          className='cursor-pointer active:scale-90'
+          onClick={() => helpers.setValue(field.value ? ++field.value : 1)}
+        />
         <input
-          className='w-full bg-second p-1 text-center	 text-xl font-bold tracking-widest	  text-tertiary focus:outline-none focus:ring focus:ring-main-opposed xs:p-2'
+          className='w-full bg-second p-1 text-center	 text-xl font-bold tracking-widest	 text-tertiary focus:outline-none focus:ring focus:ring-main-opposed xs:p-2'
           type='string'
           placeholder={"00"}
           {...field}
         />
-        <FaChevronDown />
+        <FaChevronDown
+          className='cursor-pointer active:scale-90'
+          onClick={() => helpers.setValue(field.value ? --field.value : 1)}
+        />
       </div>
-   
     </>
   );
 }
