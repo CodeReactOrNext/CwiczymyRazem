20f2e7f888ad65b1b694ddc0eeb5d300d6100850
Damian Sobieraj <damiansobieraj5@gmail.com>
2022-12-27 15:26:02 +0100

    feat: added input and logic for showing the input

---
+++ pages/api/user/avatar/index.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    console.log(req.body);
+  }
+  res.status(400);
+}
