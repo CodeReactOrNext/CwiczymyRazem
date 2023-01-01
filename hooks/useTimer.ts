a753d0c16e2eb03fadc666dc527eb4470d45295a
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2023-01-01 14:14:35 +0100

    feat: time check in mobile devices

--- hooks/useTimer.ts
+++ hooks/useTimer.ts
@@ -2,9 +2,15 @@ import { useState, useEffect, useCallback } from "react";
 
 const useTimer = () => {
   const [time, setTime] = useState(0);
+  const [startTimeData, setStartTimeData] = useState(0);
   const [timerEnabled, setTimerEnabled] = useState(false);
 
   const counter = useCallback(() => {
+    const timeDiffrence = new Date().getMilliseconds() - startTimeData;
+    if (timeDiffrence > 1000) {
+      setTime((prev) => prev + timeDiffrence);
+      return;
+    }
     setTime((prev) => prev + 1000);
   }, []);
 
@@ -14,6 +20,7 @@ const useTimer = () => {
 
   useEffect(() => {
     if (!timerEnabled) return;
+    setStartTimeData(new Date().getMilliseconds());
     const time = setInterval(() => counter(), 1000);
     return () => clearInterval(time);
   }, [counter, timerEnabled]);
