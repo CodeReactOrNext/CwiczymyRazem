f8ae6a5438009dada9689ca881a205f4e7e6381f
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2023-01-02 11:19:07 +0100

    feat: add max lenght to nick

--- feature/user/view/SettingsView/Settings.schemas.ts
+++ feature/user/view/SettingsView/Settings.schemas.ts
@@ -1,7 +1,7 @@
 import * as yup from "yup";
 
 export const updateCredsSchema = yup.object().shape({
-  login: yup.string().min(3).required("yup_errors:required"),
+  login: yup.string().min(3).max(28).required("yup_errors:required"),
   email: yup.string().email("yup_errors:valid_email"),
   password: yup.string().min(8, "yup_errors:password_char_number"),
   repeat_password: yup
