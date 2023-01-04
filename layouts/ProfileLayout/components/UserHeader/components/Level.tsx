5782f69376b54437a02bf8b29b73f43b31b60524
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2023-01-04 16:51:55 +0100

    feat: bold user lvl in profile

--- layouts/ProfileLayout/components/UserHeader/components/Level.tsx
+++ layouts/ProfileLayout/components/UserHeader/components/Level.tsx
@@ -16,12 +16,13 @@ export const Level = ({
   return (
     <div className='col-span-2 flex w-[90%] flex-col items-center text-tertiary md:col-auto lg:w-64 lg:justify-self-end xl:w-80'>
       <p>
-        {t("your_level")} <span className='text-4xl text-mainText'>{lvl}</span>
+        {t("your_level")}{" "}
+        <span className='text-4xl font-bold text-mainText'>{lvl}</span>
       </p>
       <div className=' flex w-full'>
         <p className=' relative left-3 z-10 text-sm'>
           {t("lvl_short")}
-          <span className='text-xl text-mainText'>{lvl}</span>
+          <span className='text-xl  text-mainText'>{lvl}</span>
         </p>
         <div className='relative flex h-4 w-full items-center bg-main-opposed bg-opacity-80'>
           <div
