bb91a510dca0c36a19446175d49dff95c88bcb10
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2023-01-08 23:45:41 +0100

    fix: inputTime goes to minus values

--- layouts/ReportFormLayout/components/InputTime.tsx
+++ layouts/ReportFormLayout/components/InputTime.tsx
@@ -4,12 +4,19 @@ import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 const InputTime = ({ name }: { name: string }) => {
   const [field, meta, helpers] = useField(name);
 
+  const addValue = (value: number) => {
+    return value ? ++value : 1;
+  };
+  const minusValue = (value: number) => {
+    if (value <= 0) return 0;
+    return value ? --value : 1;
+  };
   return (
     <>
       <div className='flex max-w-[3rem] flex-col items-center gap-y-2'>
         <button
           type='button'
-          onClick={() => helpers.setValue(field.value ? ++field.value : 1)}
+          onClick={() => helpers.setValue(addValue(field.value))}
           className='active:click-behavior-second'>
           <FaChevronUp />
         </button>
@@ -21,7 +28,7 @@ const InputTime = ({ name }: { name: string }) => {
         />
         <button
           type='button'
-          onClick={() => helpers.setValue(field.value ? --field.value : 1)}
+          onClick={() => helpers.setValue(minusValue(field.value))}
           className='active:click-behavior-second'>
           <FaChevronDown />
         </button>
