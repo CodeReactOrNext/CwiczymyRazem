d82a5def7c0d9a12298b1e9da22cc5d047afffe4
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2023-01-16 21:56:35 +0100

    feat: new font ProfileLanding

--- layouts/ProfileLayout/components/StatisticBar.tsx
+++ layouts/ProfileLayout/components/StatisticBar.tsx
@@ -11,14 +11,14 @@ const StatisticBar = ({ title, value, percent }: StatisticBarProps) => {
       <p className='text-sm sm:text-lg'>{percentValue}%</p>
       <div className='flex h-32 w-5 bg-main-opposed-50 bg-opacity-50 sm:h-40 sm:w-8 '>
         <div
-          className='w-5  self-end  bg-gradient-to-t from-main-700 to-main-300 sm:w-8'
+          className='w-5  self-end  bg-gradient-to-t from-main-700 to-main-300 sm:w-8 '
           style={{ height: percentValue + "%" }}
         />
       </div>
-      <p className='my-2 w-full bg-main-opposed bg-opacity-80  px-5 sm:px-10 md:px-5 xl:px-8'>
+      <p className='my-2 w-full bg-main-opposed bg-opacity-80 px-5 font-openSans text-sm  font-bold sm:px-10 md:px-5 xl:px-8 '>
         {value}h
       </p>
-      <p>{title}</p>
+      <p className='font-openSans text-sm font-bold'>{title}</p>
     </div>
   );
 };
