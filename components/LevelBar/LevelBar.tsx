63c51dc871c8ed1cebda0d7594b1d6ba5671ce0f
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2023-01-23 18:25:31 +0100

    feat: new layout menu flow

--- components/LevelBar/LevelBar.tsx
+++ components/LevelBar/LevelBar.tsx
@@ -5,23 +5,23 @@ interface LevelInterfaceProps {
   lvl: number;
   currentLevelMaxPoints: number;
 }
-const Level = ({
+const LevelBar = ({
   points,
   lvl,
   currentLevelMaxPoints: pointsToNextLvl,
 }: LevelInterfaceProps) => {
-  const { t } = useTranslation("profile");
+  const { t } = useTranslation("common");
   const progressPercent = (points / pointsToNextLvl) * 100;
 
   return (
     <div className='col-span-2 m-auto flex w-[90%] flex-col items-center text-tertiary md:col-auto lg:w-64 lg:justify-self-end xl:w-80 '>
       <p>
-        {t("your_level")}{" "}
+        {t("header.your_level")}{" "}
         <span className='text-4xl font-bold text-mainText'>{lvl}</span>
       </p>
       <div className=' flex w-full'>
         <p className=' relative left-3 z-10 text-sm'>
-          {t("lvl_short")}
+          {t("header.lvl_short")}
           <span className='text-xl  text-mainText'>{lvl}</span>
         </p>
         <div className='relative flex h-4 w-full items-center bg-main-opposed bg-opacity-80 radius-default'>
@@ -30,16 +30,16 @@ const Level = ({
             style={{ width: progressPercent + "%" }}></div>
         </div>
         <p className='relative right-3 text-sm'>
-          {t("lvl_short")}
+          {t("header.lvl_short")}
           <span className='text-xl  text-mainText'>{lvl + 1}</span>
         </p>
       </div>
       <p>
         {points}/{pointsToNextLvl}
-        {t("points_short")}
+        {t("header.points_short")}
       </p>
     </div>
   );
 };
 
-export default Level;
+export default LevelBar;
--- layouts/MainLoggedLayout/components/UserHeader/UserHeader.tsx
+++ layouts/MainLoggedLayout/components/UserHeader/UserHeader.tsx
@@ -1,11 +1,12 @@
 import Logo from "components/Logo";
-import Level from "./components/Level";
 import Avatar from "components/Avatar";
+import LevelBar from "components/LevelBar";
 import UserNav from "components/UserNav";
 import NavDecoration from "./components/NavDecoration";
 import WelcomeMessage from "./components/WelcomMessage";
 
 import { StatisticsDataInterface } from "constants/userStatisticsInitialData";
+
 interface UserHeaderProps {
   userStats: StatisticsDataInterface;
   userName: string;
@@ -33,7 +34,7 @@ const UserHeader = ({ userStats, userName, avatar }: UserHeaderProps) => {
         </div>
       </div>
       <NavDecoration />
-      <Level
+      <LevelBar
         points={points}
         lvl={lvl}
         currentLevelMaxPoints={currentLevelMaxPoints}
