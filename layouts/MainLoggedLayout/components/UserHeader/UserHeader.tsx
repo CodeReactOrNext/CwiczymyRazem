e797e4946473a0ed21d1fc63b3230b3e9fbd4334
DevmianS <damiansobierajdev@gmail.com>
2023-01-24 13:35:17 +0100

    fix: fixed flame covering log off button in landing view and fixed WelcomeMessage component name typo

--- layouts/MainLoggedLayout/components/UserHeader/UserHeader.tsx
+++ layouts/MainLoggedLayout/components/UserHeader/UserHeader.tsx
@@ -3,7 +3,7 @@ import Avatar from "components/Avatar";
 import LevelBar from "components/LevelBar";
 import UserNav from "components/UserNav";
 import NavDecoration from "./components/NavDecoration";
-import WelcomeMessage from "./components/WelcomMessage";
+import WelcomeMessage from "./components/WelcomeMessage";
 
 import { StatisticsDataInterface } from "constants/userStatisticsInitialData";
 
@@ -17,7 +17,7 @@ const UserHeader = ({ userStats, userName, avatar }: UserHeaderProps) => {
   const { points, lvl, currentLevelMaxPoints, lastReportDate } = userStats;
   return (
     <>
-      <div className='flex flex-col items-start space-x-2 space-y-2 text-lg '>
+      <div className='z-30 flex flex-col items-start space-x-2 space-y-2 text-lg '>
         <div className='flex flex-row items-center gap-5 sm:gap-10'>
           <div className='scale-75 sm:scale-100 lg:mr-4'>
             <Logo />
