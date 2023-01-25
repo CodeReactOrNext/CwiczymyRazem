b1dc2419f96c41eb5695a5b6edc098987bd14ab6
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2023-01-25 18:36:33 +0100

    fix: theme locals and add missing copy link in welcome msg

--- Hoc/ThemeModeProvider.tsx
+++ Hoc/ThemeModeProvider.tsx
@@ -1,8 +1,20 @@
-import { selectLayoutMode } from "feature/user/store/userSlice";
-import { useAppSelector } from "store/hooks";
+import { changeTheme, selectLayoutMode } from "feature/user/store/userSlice";
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "store/hooks";
 
 const ThemeModeProvider = ({ children }: { children: React.ReactNode }) => {
+  const dispatch = useAppDispatch();
   const theme = useAppSelector(selectLayoutMode);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      if (localStorage.getItem("userSlice.theme")) {
+        dispatch(
+          changeTheme(JSON.parse(localStorage.getItem("userSlice.theme")!))
+        );
+      }
+    }
+  }, []);
   return <div className={theme}> {children}</div>;
 };
 
