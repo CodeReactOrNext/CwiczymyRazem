745768b5cc33dff531f7db6ed5aa5cbb7a077f42
DevmianS <damiansobierajdev@gmail.com>
2023-02-06 19:24:58 +0100

    fix: fixed 'Avoid non-composited animations'

--- layouts/HeroLayout/components/Guitar/Guitar.tsx
+++ layouts/HeroLayout/components/Guitar/Guitar.tsx
@@ -1,17 +1,18 @@
-/* eslint-disable @next/next/no-img-element */
 import LightningSVG from "public/static/images/svg/Lightning";
 import LightningRevSVG from "public/static/images/svg/LightningRev";
+import Image from "next/future/image";
 
 const Guitar = () => {
   return (
     <div className='h-full w-full'>
       <div className='absolute left-[50%] -bottom-[10%] z-40 h-fit w-[20%] max-w-[100px] -translate-x-[50%] lg:bottom-[15%] lg:w-[35%] lg:max-w-max xl:w-[25%]'>
         <div className='shadow-animation absolute -bottom-[15%] aspect-square w-full rounded-full'></div>
-        <img
+        <Image
           className='hover-animation w-full'
           src='/static/images/guitar_red.png'
-          alt=''
-        />
+          width={366}
+          height={1091}
+          alt=''></Image>
         <div className='shake-animation absolute -bottom-[5%] -left-[100%] w-[65%] lg:-bottom-[15%]'>
           <LightningRevSVG className='origin-right rotate-[140deg] fill-tertiary-500 ' />
         </div>
