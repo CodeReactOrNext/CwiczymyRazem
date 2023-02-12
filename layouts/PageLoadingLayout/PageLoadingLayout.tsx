3d50c7d1d88c15593accad932f17abbb7b3b7e94
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2023-02-12 14:08:26 +0100

    feat/fix: add external link, fix numer in pageLoadingLayout and fix infinite loading when user is not log in on FAQ/Leaderboard

--- layouts/PageLoadingLayout/PageLoadingLayout.tsx
+++ layouts/PageLoadingLayout/PageLoadingLayout.tsx
@@ -3,7 +3,7 @@ import { CircleSpinner } from "react-spinners-kit";
 const PageLoadingLayout = () => {
   return (
     <div className=' flex h-[40vh] w-full items-center justify-center'>
-      <CircleSpinner size='60' />
+      <CircleSpinner size={60} />
     </div>
   );
 };
