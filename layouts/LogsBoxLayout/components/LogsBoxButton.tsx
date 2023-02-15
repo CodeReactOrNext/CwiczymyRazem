d7debe1f6844facb19f9b8d0667d76465738d936
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2023-02-15 16:16:49 +0100

    feat: add discor event

--- layouts/LogsBoxLayout/components/LogsBoxButton.tsx
+++ layouts/LogsBoxLayout/components/LogsBoxButton.tsx
@@ -1,16 +1,25 @@
+import { IconType } from "react-icons/lib";
+
 interface LogsBoxButtonProps {
   title: string;
   active: boolean;
   onClick: () => void;
+  Icon: IconType;
 }
 
-const LogsBoxButton = ({ title, active, onClick }: LogsBoxButtonProps) => {
+const LogsBoxButton = ({
+  title,
+  active,
+  onClick,
+  Icon,
+}: LogsBoxButtonProps) => {
   return (
     <button
-      className={`bg-opacity-80 p-2 radius-default hover:bg-main-opposed-100
+      className={`flex flex-row items-center bg-opacity-80 p-2 radius-default hover:bg-main-opposed-100
       ${active ? "bg-main-opposed-200 " : ""}`}
       onClick={onClick}>
-      {title}
+      <Icon className='mx-1 text-base  xs:text-2xl sm:text-base' />
+      <span className='hidden sm:block'> {title}</span>
     </button>
   );
 };
