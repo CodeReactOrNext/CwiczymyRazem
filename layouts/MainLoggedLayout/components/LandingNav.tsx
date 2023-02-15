e58ac52bf0a767869e12a6e3d1948b7ec090327f
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2023-02-15 03:30:58 +0100

    feat: external link in LandingNav

--- layouts/MainLoggedLayout/components/LandingNav.tsx
+++ layouts/MainLoggedLayout/components/LandingNav.tsx
@@ -5,6 +5,7 @@ export interface LandingNavLinkInterface {
   id: NavPagesTypes;
   name: string;
   href: string;
+  external?: boolean;
 }
 
 export interface LandingNavObjectInterface {
@@ -26,22 +27,24 @@ const LandingNav = ({
   return (
     <div className='relative flex w-full justify-around bg-tertiary py-2 text-base uppercase text-main-opposed-text sm:text-3xl md:gap-x-[150px] xl:gap-x-[250px]'>
       <div className='z-40 flex h-full w-full justify-around'>
-        {leftSideLinks.map(({ href, name, id }) => (
+        {leftSideLinks.map(({ href, name, id, external }) => (
           <NavLink
             key={id}
             href={href}
             name={name}
             isCurrentPage={id === pageId}
+            external
           />
         ))}
       </div>
       <div className='z-40  flex w-full  justify-around'>
-        {rightSideLinks.map(({ href, name, id }) => (
+        {rightSideLinks.map(({ href, name, id, external }) => (
           <NavLink
             key={id}
             href={href}
             name={name}
             isCurrentPage={id === pageId}
+            external
           />
         ))}
       </div>
--- layouts/MainLoggedLayout/components/NavLink.tsx
+++ layouts/MainLoggedLayout/components/NavLink.tsx
@@ -4,21 +4,24 @@ interface NavLinkProps {
   href: string;
   name: string;
   isCurrentPage: boolean;
+  external?: boolean;
 }
 
-const NavLink = ({ href, name, isCurrentPage }: NavLinkProps) => {
+const NavLink = ({ href, name, isCurrentPage, external }: NavLinkProps) => {
   return (
     <Link href={href}>
-      <button
-        disabled={isCurrentPage}
-        className={`active:click-behavio p-1 px-2 sm:px-4 radius-default  
+      <a target={external ? "_blank" : undefined} rel='noopener noreferrer'>
+        <button
+          disabled={isCurrentPage}
+          className={`active:click-behavio p-1 px-2 radius-default sm:px-4  
             ${
               isCurrentPage
                 ? " bg-second-500 text-mainText shadow-sm"
                 : " click-behavior hover:bg-second-500 hover:text-mainText"
             } `}>
-        {name}
-      </button>
+          {name}
+        </button>
+      </a>
     </Link>
   );
 };
