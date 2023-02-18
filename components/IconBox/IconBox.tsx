6db3dbc43be7e57c98cf3544749a8c1cf300cb0b
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2023-02-18 12:13:33 +0100

    fix: glitch in mobile

--- components/IconBox/IconBox.tsx
+++ components/IconBox/IconBox.tsx
@@ -1,4 +1,3 @@
-import { FaSadCry } from "react-icons/fa";
 import { IconType } from "react-icons/lib";
 
 interface IconBoxProps {
@@ -7,8 +6,8 @@ interface IconBoxProps {
 
 const IconBox = ({ Icon }: IconBoxProps) => {
   return (
-    <div className='m-2 flex h-7 w-7 shrink-0 items-center justify-center bg-main-200 text-mainText  shadow-main-700 radius-default sm:h-10 sm:w-10 sm:text-lg'>
-      <FaSadCry />
+    <div className='m-2 flex h-7 w-7  items-center justify-center bg-main-200 text-mainText  shadow-main-700 radius-default sm:h-10 sm:w-10 sm:text-lg'>
+      <Icon />
     </div>
   );
 };
