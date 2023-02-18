cda96005ff3c15ae1e38ca3da6b723a2adc6eb6b
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2023-02-18 12:17:01 +0100

    fix: glitch in mobile

--- components/IconBox/IconBox.tsx
+++ components/IconBox/IconBox.tsx
@@ -7,7 +7,7 @@ interface IconBoxProps {
 const IconBox = ({ Icon }: IconBoxProps) => {
   return (
     <div className='m-2 flex h-7 w-7  items-center justify-center bg-main-200 text-mainText  shadow-main-700 radius-default sm:h-10 sm:w-10 sm:text-lg'>
-      <Icon />
+ D
     </div>
   );
 };
