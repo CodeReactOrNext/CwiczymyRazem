b02b18eb10a99ff8adecd3fb519bc71b5ebf93a0
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2023-03-01 21:30:59 +0100

    fix: leadboard resolution and mainLayout two scrolls

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -4,24 +4,17 @@ import Background from "./components/Background";
 import ContentBox from "./components/ContentBox";
 import SubtitleBar from "./components/SubtitleBar";
 
-
 export type layoutVariant = "primary" | "secondary" | "landing";
 
 interface MainLayoutProps {
   children: React.ReactNode;
   subtitle: string;
   variant: layoutVariant;
-
 }
 
-const MainHeroLayout = ({
-  children,
-  subtitle,
-  variant,
-}: MainLayoutProps) => {
+const MainHeroLayout = ({ children, subtitle, variant }: MainLayoutProps) => {
   return (
-    <main
-      className='h-screen bg-tertiary-bg  font-sans  lg:min-h-[920px] 2xl:p-4'>
+    <main className='h-screen bg-tertiary-bg  font-sans  2xl:p-4'>
       <div className='relative flex h-full w-full items-center justify-center '>
         <div
           className={`relative  flex h-full max-h-[calc(1200px_-_4rem)] w-full max-w-[2200px] flex-col overflow-x-hidden scrollbar-hide ${
