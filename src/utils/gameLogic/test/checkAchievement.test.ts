aec3b427f13cf67168563cc1b70f341a25655778
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2023-03-17 15:03:53 +0100

    feat: cheacAchievement test

--- src/utils/gameLogic/test/checkAchievement.test.ts
+++ src/utils/gameLogic/test/checkAchievement.test.ts
@@ -8,69 +8,68 @@ import { describe, expect, it } from "vitest";
 import { checkAchievement } from "../checkAchievement";
 
 describe("checkAchievement", () => {
+  const emptyReportData: ReportDataInterface = {
+    reportDate: new Date(),
+    totalPoints: 0,
+    bonusPoints: {
+      multiplier: 0,
+      habitsCount: 0,
+      additionalPoints: 0,
+      time: 0,
+      timePoints: 0,
+    },
+  };
+  const emptyInputData: ReportFormikInterface = {
+    techniqueHours: "0",
+    techniqueMinutes: " 0",
+    theoryHours: "0",
+    theoryMinutes: "0",
+    hearingHours: "0",
+    hearingMinutes: "0",
+    creativityHours: "0",
+    creativityMinutes: "0",
+    countBackDays: 0,
+    reportTitle: "0",
+    habbits: [],
+  };
+
   it("returns the empty array (don' give you any achievements) if you have empty stats", () => {
     const statistics = statisticsInitial;
-    const reportData: ReportDataInterface = {
-      reportDate: new Date(),
-      totalPoints: 0,
-      bonusPoints: {
-        multiplier: 0,
-        habitsCount: 0,
-        additionalPoints: 0,
-        time: 0,
-        timePoints: 0,
-      },
-    };
-    const inputData: ReportFormikInterface = {
-      techniqueHours: "0",
-      techniqueMinutes: " 0",
-      theoryHours: "0",
-      theoryMinutes: "0",
-      hearingHours: "0",
-      hearingMinutes: "0",
-      creativityHours: "0",
-      creativityMinutes: "0",
-      countBackDays: 0,
-      reportTitle: "0",
-      habbits: [],
-    };
+    const achievement = checkAchievement(
+      statistics,
+      emptyReportData,
+      emptyInputData
+    );
 
-    const achievement = checkAchievement(statistics, reportData, inputData);
     expect(achievement).toEqual([]);
   });
 
-  it("returns same achievements if you don't make any progress ", () => {
+  it("returns achievements id's in array if you if you meet the conditions ", () => {
     const statistics: StatisticsDataInterface = {
       ...statisticsInitial,
-      achievements: ["event", "artist", "doctor"],
+      points: 10000,
     };
+    const achievement = checkAchievement(
+      statistics,
+      emptyReportData,
+      emptyInputData
+    );
 
-    const reportData: ReportDataInterface = {
-      reportDate: new Date(),
-      totalPoints: 0,
-      bonusPoints: {
-        multiplier: 0,
-        habitsCount: 0,
-        additionalPoints: 0,
-        time: 0,
-        timePoints: 0,
-      },
-    };
-    const inputData: ReportFormikInterface = {
-      techniqueHours: "0",
-      techniqueMinutes: " 0",
-      theoryHours: "0",
-      theoryMinutes: "0",
-      hearingHours: "0",
-      hearingMinutes: "0",
-      creativityHours: "0",
-      creativityMinutes: "0",
-      countBackDays: 0,
-      reportTitle: "0",
-      habbits: [],
-    };
+    expect(achievement).toEqual(["points_1", "points_2", "points_3"]);
+  });
 
-    const achievement = checkAchievement(statistics, reportData, inputData);
-    expect(achievement).toEqual(["event", "artist", "doctor"]);
+  it("returns only id's achievements that you don't own ", () => {
+    const statistics: StatisticsDataInterface = {
+      ...statisticsInitial,
+      points: 10000,
+      achievements: ["points_1", "points_2"],
+    };
+    const achievement = checkAchievement(
+      statistics,
+      emptyReportData,
+      emptyInputData
+    );
+
+    expect(achievement).toEqual(["points_3"]);
   });
 });
