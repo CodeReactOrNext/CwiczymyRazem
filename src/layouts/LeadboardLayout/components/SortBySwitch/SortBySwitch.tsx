73a67b13bab9ef2214c62052e714be665760b99c
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2025-01-02 23:48:17 +0100

    feat: UI mobile improvment

--- src/layouts/LeadboardLayout/components/SortBySwitch/SortBySwitch.tsx
+++ src/layouts/LeadboardLayout/components/SortBySwitch/SortBySwitch.tsx
@@ -11,29 +11,33 @@ function SortBySwitch({ setSortBy, sortBy }: SortBySwitchInterface) {
   const { t } = useTranslation("common");
 
   return (
-    <div className='flex  items-center justify-center gap-2 rounded-md bg-second-300 p-1 px-2 font-openSans text-xs text-mainText xs:text-sm sm:text-base  '>
-      <p className=' text-[12px] text-secondText'>{t("sort.sort_by")}</p>
-      <button
-        onClick={() => setSortBy("points")}
-        className={`tab rounded-md text-[12px] font-medium transition-colors hover:text-white ${
-          sortBy === "points" ? "bg-second text-white" : " "
-        }`}>
-        {t("sort.points")}
-      </button>
-      <button
-        onClick={() => setSortBy("time")}
-        className={`tab rounded-md text-[12px] font-medium transition-colors hover:text-white ${
-          sortBy === "time" ? "bg-second text-white" : " "
-        }`}>
-        {t("sort.time")}
-      </button>
-      <button
-        onClick={() => setSortBy("sessionCount")}
-        className={`tab rounded-md text-[12px] font-medium transition-colors hover:text-white ${
-          sortBy === "sessionCount" ? "bg-second text-white" : " "
-        }`}>
-        {t("sort.session_count")}
-      </button>
+    <div className='flex w-full flex-col justify-center gap-2  rounded-md bg-second-300 p-1 px-2 font-openSans text-xs text-mainText xs:text-sm sm:text-base md:w-fit md:flex-row md:items-center  '>
+      <p className='ml-3 text-[12px] text-secondText md:ml-0 '>
+        {t("sort.sort_by")}
+      </p>
+      <div>
+        <button
+          onClick={() => setSortBy("points")}
+          className={`tab rounded-md text-[12px] font-medium transition-colors hover:text-white ${
+            sortBy === "points" ? "bg-second text-white" : " "
+          }`}>
+          {t("sort.points")}
+        </button>
+        <button
+          onClick={() => setSortBy("time")}
+          className={`tab rounded-md text-[12px] font-medium transition-colors hover:text-white ${
+            sortBy === "time" ? "bg-second text-white" : " "
+          }`}>
+          {t("sort.time")}
+        </button>
+        <button
+          onClick={() => setSortBy("sessionCount")}
+          className={`tab rounded-md text-[12px] font-medium transition-colors hover:text-white ${
+            sortBy === "sessionCount" ? "bg-second text-white" : " "
+          }`}>
+          {t("sort.session_count")}
+        </button>
+      </div>
     </div>
   );
 }
