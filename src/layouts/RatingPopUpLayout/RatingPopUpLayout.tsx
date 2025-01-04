3e856d0fd54c8e3292a6e694f3a904b37bc4a8f7
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2025-01-04 18:34:38 +0100

    feat: ui report improvment

--- src/layouts/RatingPopUpLayout/RatingPopUpLayout.tsx
+++ src/layouts/RatingPopUpLayout/RatingPopUpLayout.tsx
@@ -40,7 +40,7 @@ const RatingPopUp = ({
   useEffect(() => {
     const timeout = setTimeout(() => {
       setCurrentLevel(currentUserStats.lvl);
-    }, 3100);
+    }, 2000);
 
     return () => {
       clearTimeout(timeout);
@@ -53,101 +53,57 @@ const RatingPopUp = ({
     (x) => !previousUserStats.achievements.includes(x)
   );
 
-  const leveledUp = () => {
-    return previousUserStats.lvl !== currentUserStats.lvl;
-  };
-
   const levelXpStart =
     currentUserStats.lvl === 1 ? 0 : getPointsToLvlUp(currentUserStats.lvl - 1);
   const levelXpEnd = getPointsToLvlUp(currentUserStats.lvl);
 
   const pointsInThisLevel = currentUserStats.points - levelXpStart;
-
   const levelXpDifference = levelXpEnd - levelXpStart;
+
   const prevProgressPercent =
     ((pointsInThisLevel - ratingData.totalPoints) / levelXpDifference) * 100;
-
   const currProgressPercent = (pointsInThisLevel / levelXpDifference) * 100;
 
   return (
-    <div className='relative flex h-5/6 max-h-[1020px] min-h-[750px] w-[95%] translate-y-[10%] items-center justify-center bg-main-opposed-500 font-sans tracking-wider  md:min-h-[800px] lg:aspect-square lg:w-auto'>
-      <OldEffect className='absolute z-10' />
-      <div className='absolute  top-[20%] -left-[5%] right-0 flex w-[110%] items-center justify-center bg-main-500 text-5xl font-medium sm:text-8xl'>
-        <p>
-          {ratingData.totalPoints}
-          {t("rating_popup.points")}
-        </p>
-      </div>
-      <p className='absolute top-[5%] text-5xl font-medium text-tertiary-500 md:text-8xl'>
-        {t("rating_popup.title")}
-      </p>
-
-      <div className='absolute right-0 left-0 bottom-0 z-10 h-[30%] max-h-[270px] w-full overflow-hidden  sm:h-[50%] md:max-h-[500px] '>
-        <FireSVG className='absolute -bottom-10 -left-[10%] w-4/6 rotate-6 fill-second-500 md:bottom-auto' />
-        <FireSVG className='absolute -bottom-10  -right-[10%] w-4/6 -rotate-6 fill-second-500 md:bottom-auto' />
-        <div className='absolute bottom-0  h-12 w-full bg-second-500'></div>
-
-        <div className='absolute bottom-0 flex h-1/4 w-full items-start justify-center'>
-          <Button
-            onClick={() => {
-              onClick(false);
-              Router.push("/");
-            }}>
-            {t("rating_popup.back")}
-          </Button>
-          <div className='absolute -top-[230%] z-40 my-auto flex h-7 w-2/5 items-center md:-top-[130%] md:w-5/12'>
-            <div className='h-4/5 w-full bg-second-500 '>
+      <div className="dialog relative">
+        <div className="modal-box p-12 bg-second-600 relative w-full lg:min-w-[800px]">
+          <Button variant="outline" className="btn btn-sm btn-circle absolute right-2 top-2">✕</Button>
+         
+          <p className="py-4 text-5xl">
+         <span className="text-lg font-openSans">{t("rating_popup.title")}</span>  <span className="text-main-300">{ratingData.totalPoints}</span> <span className="text-4xl">{t("rating_popup.points")}</span>
+          </p>
+          <BonusPointsItem
+            bonusPoints={ratingData.bonusPoints}
+            actualDayWithoutBreak={currentUserStats.actualDayWithoutBreak}
+            achievements={newAchievements}
+            isGetNewLevel={isGetNewLevel}
+          />
+          <div className=" w-[80%] flex mx-auto justify-center">
+            <LevelIndicator>{currentLevel}</LevelIndicator>
+            <div className="relative mx-2 h-4 w-full bg-second-500">
               <motion.div
-                initial={{
-                  width: `${
-                    !leveledUp()
-                      ? prevProgressPercent
-                      : previousUserStats.points === 0
-                      ? 0
-                      : 100
-                  }%`,
-                }}
-                animate={{
-                  width: `${currProgressPercent}%`,
-                }}
-                transition={{ duration: 2, delay: leveledUp() ? 3 : 1 }}
-                className={`absolute left-0 top-0 h-full bg-main-500 bg-gradient-to-r from-main-700 to-main-300`}>
-                <motion.p
-                  initial={{ opacity: 0, y: 20, x: "50%" }}
-                  animate={{ opacity: 1, y: 0 }}
-                  transition={{ ease: "easeOut", duration: 0.3, delay: 0.9 }}
-                  className='absolute right-0 -top-[80%] overflow-hidden whitespace-nowrap text-lg font-medium text-second-text md:text-xl'>
-                  +{ratingData.totalPoints} {t("rating_popup.points")}
-                </motion.p>
-              </motion.div>
+                initial={{ width: `${prevProgressPercent}%` }}
+                animate={{ width: `${currProgressPercent}%` }}
+                transition={{ duration: 1.5 }}
+                className="absolute left-0 top-0 h-full rounded-md bg-main-500"
+              />
             </div>
-            <LevelIndicator position='left'>{currentLevel}</LevelIndicator>
-            <LevelIndicator position='right'>{currentLevel + 1}</LevelIndicator>
+            <LevelIndicator>{currentLevel + 1}</LevelIndicator>
+          </div>
+          
+          <div className="modal-action">
+            <Button
+              onClick={() => {
+                onClick(false);
+                Router.push("/");
+              }}
+              className="btn-primary"
+            >
+              {t("rating_popup.back")}
+            </Button>
           </div>
         </div>
       </div>
-      <BonusPointsItem
-        bonusPoints={ratingData.bonusPoints}
-        actualDayWithoutBreak={currentUserStats.actualDayWithoutBreak}
-        achievements={newAchievements}
-        isGetNewLevel={isGetNewLevel}
-      />
-      <div className='absolute -bottom-[15%] -left-[27%] z-40 w-[50%] sm:-left-[15%] md:-bottom-[5%] md:-left-[10%] md:w-auto'>
-        <Image
-          src={blackGuitar}
-          height={705}
-          width={254}
-          objectFit='contain'
-          alt=''></Image>
-      </div>
-      <motion.div
-        className='absolute -right-[25%] -top-[25%] z-10  h-5/6 w-[50%] -rotate-12 md:-top-[5%] lg:-right-[25%]'
-        initial={{ y: "-100%" }}
-        animate={{ y: 0 }}
-        transition={{ delay: 0.3, type: "tween" }}>
-        <Lightning className='absolute -rotate-12 fill-tertiary-500' />
-      </motion.div>
-    </div>
   );
 };
 
