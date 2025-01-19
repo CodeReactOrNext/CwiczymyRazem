54c4c63a9de6da08960e2adebc686f3b4d05aef0
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2025-01-19 19:57:19 +0100

    change component on leadearboard

--- src/feature/leadboard/components/ViewToggle/ViewToggle.tsx
+++ src/feature/leadboard/components/ViewToggle/ViewToggle.tsx
@@ -14,23 +14,25 @@ const ViewToggle = ({
   const { t } = useTranslation("leadboard");
 
   return (
-    <div className='join'>
-      <button
-        className={`btn join-item btn-sm ${
-          !isSeasonalView ? "btn-primary" : ""
-        }`}
-        onClick={() => setIsSeasonalView(false)}
-        disabled={isLoading}>
-        {t("global_leaderboard")}
-      </button>
-      <button
-        className={`btn join-item btn-sm ${
-          isSeasonalView ? "btn-primary" : ""
-        }`}
-        onClick={() => setIsSeasonalView(true)}
-        disabled={isLoading}>
-        {t("seasonal_leaderboard")}
-      </button>
+    <div className='flex items-center gap-2 font-openSans'>
+      <div className='flex gap-2 rounded-md bg-second-400 p-1'>
+        <button
+          className={`tab rounded-md text-[12px] font-medium transition-colors hover:text-white  ${
+            !isSeasonalView ? "bg-second text-white" : " "
+          }`}
+          onClick={() => setIsSeasonalView(false)}
+          disabled={isLoading}>
+          {t("global_leaderboard")}
+        </button>
+        <button
+          className={` tab rounded-md text-[12px] font-medium transition-colors hover:text-white  ${
+            isSeasonalView ? "bg-second text-white" : " "
+          }`}
+          onClick={() => setIsSeasonalView(true)}
+          disabled={isLoading}>
+          {t("seasonal_leaderboard")}
+        </button>
+      </div>
     </div>
   );
 };
