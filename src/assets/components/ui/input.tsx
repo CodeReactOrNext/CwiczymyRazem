a748fcc7fdc59c0d5a247ca8edb803e8cc1a366e
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2025-01-23 00:00:19 +0100

    feat(Input): enhance input component with start and end icons; update SongsView to utilize new icons and loading state

--- src/assets/components/ui/input.tsx
+++ src/assets/components/ui/input.tsx
@@ -1,18 +1,41 @@
 import { cn } from "assets/lib/utils";
 import * as React from "react";
 
-const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
-  ({ className, type, ...props }, ref) => {
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  startIcon?: JSX.Element;
+  endIcon?: JSX.Element;
+}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, startIcon, endIcon, ...props }, ref) => {
+    const StartIcon = startIcon;
+    const EndIcon = endIcon;
+
     return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 font-openSans text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-          className
+      <div className='relative w-full'>
+        {StartIcon && (
+          <div className='absolute left-1.5 top-1/2 -translate-y-1/2 transform'>
+            {startIcon}
+          </div>
         )}
-        ref={ref}
-        {...props}
-      />
+        <input
+          type={type}
+          className={cn(
+            "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 font-openSans text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+            startIcon ? "pl-8" : "",
+            endIcon ? "pr-8" : "",
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+        {EndIcon && (
+          <div className='absolute right-3 top-1/2 -translate-y-1/2 transform'>
+            {endIcon}
+          </div>
+        )}
+      </div>
     );
   }
 );
