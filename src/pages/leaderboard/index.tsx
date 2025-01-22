e508cf033e68c80b12ebe3eba088c348690d8a10
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2025-01-22 19:52:38 +0100

    feat(LeaderboardPage): implement auto-login redirection

--- src/pages/leaderboard/index.tsx
+++ src/pages/leaderboard/index.tsx
@@ -2,14 +2,23 @@ import type { NextPage } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import LeadboardView from "feature/leadboard/view/LeadboardView";
 import AuthLayoutWrapper from "wrappers/AuthLayoutWrapper";
+import useAutoLogIn from "hooks/useAutoLogIn";
+import PageLoadingLayout from "layouts/PageLoadingLayout";
+import LoginView from "feature/user/view/LoginView";
 
 const LeaderBoardPage: NextPage = () => {
+  const { isLoggedIn } = useAutoLogIn({
+    redirects: {
+      loggedOut: "/login",
+    },
+  });
+
   return (
     <AuthLayoutWrapper
       pageId={"leadboard"}
       subtitle='Leaderboard'
       variant='secondary'>
-      <LeadboardView />
+      {isLoggedIn ? <LeadboardView /> : <PageLoadingLayout />}
     </AuthLayoutWrapper>
   );
 };
