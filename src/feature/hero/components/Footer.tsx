1082b6d14d589740044e106de3efab289bb0228a
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2025-03-30 19:03:29 +0200

    refactor(hero): restructure HeroView component by modularizing subcomponents for better organization

--- src/feature/hero/components/Footer.tsx
+++ src/feature/hero/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='absolute bottom-0 w-full'>
-      <p className='text-center font-openSans text-sm text-mainText'>
+      <p className='text-center font-openSans text-[12px] text-mainText'>
         {children}
       </p>
     </div>
