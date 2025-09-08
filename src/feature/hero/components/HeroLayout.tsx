ec4bc4e7f8c34bd852db10eb9a2fbfac45677be0
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2025-09-08 16:00:07 +0200

    refactor(Assets): remove unused Fire and FireDouble SVG components to streamline asset management and reduce bundle size; update HeadDecoration and MainContainer components to reflect these changes and enhance layout and styling for improved user experience.

--- src/feature/hero/components/HeroLayout.tsx
+++ src/feature/hero/components/HeroLayout.tsx
@@ -4,7 +4,7 @@ import OldEffect from "components/OldEffect";
 import Guitar from "feature/hero/components/Guitar";
 import { HeroSlogan } from "feature/hero/components/HeroSlogan";
 import Link from "next/link";
-import FireDoubleSVG from "public/static/images/svg/Fire_double";
+
 import { useTranslation } from "react-i18next";
 
 interface HeroProps {
@@ -18,15 +18,12 @@ export const HeroLayout = ({ children, buttonOnClick }: HeroProps) => {
   return (
     <div className='grid h-full w-full grid-cols-1 grid-rows-2 gap-[20%] lg:grid-cols-[4fr_5fr] lg:grid-rows-1'>
       <div className='relative h-full w-full'>
-        <div className='absolute bottom-0 top-0 h-full w-full '>
-          <FireDoubleSVG className='absolute left-0 right-0 top-[60%] w-[110%] -translate-x-[7%] fill-second-600 sm:top-[50%] md:top-[50%] lg:-bottom-[45%] lg:top-auto lg:h-full xl:-bottom-[50%]' />
-        </div>
         <HeroSlogan />
         <OldEffect />
         <Guitar />
       </div>
       <div className='z-30 flex flex-col items-center justify-center gap-6  lg:-mb-24 lg:flex-col xl:pr-8 '>
-        <span className='px-4 text-left font-openSans text-xs font-bold text-mainText xs:text-sm sm:text-base lg:text-right '>
+        <span className='font-openSans px-4 text-left text-xs font-bold text-mainText xs:text-sm sm:text-base lg:text-right '>
           {children}
         </span>
         <Link href='/login'>
