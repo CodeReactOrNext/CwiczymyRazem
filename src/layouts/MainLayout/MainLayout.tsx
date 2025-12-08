9b76a2c68dc5f7308a1d29f4f19fbf6f95d88d83
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2025-12-08 23:24:31 +0100

    feat: introduce MainHeroLayout component for primary application layout

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -14,22 +14,9 @@ interface MainLayoutProps {
 
 const MainHeroLayout = ({ children, subtitle, variant }: MainLayoutProps) => {
   return (
-    <main className='h-screen  bg-tertiary-bg font-sans 2xl:p-4 '>
-      <div className='relative flex h-full w-full items-center justify-center '>
-        <div className='relative flex h-full  w-full max-w-[2200px] flex-col overflow-y-auto  overflow-x-hidden  bg-main-opposed-bg scrollbar-hide'>
-          <div
-            className={`relative  flex h-full  w-full max-w-[2200px] flex-col overflow-x-hidden scrollbar-hide ${
-              variant === "secondary" ? "bg-second-600" : "bg-main-opposed-600"
-            }`}>
-            <Header variant={variant} />
-            <SubtitleBar variant={variant}>{subtitle}</SubtitleBar>
-            <div className='relative z-10   flex h-full w-full items-center justify-center lg:m-0'>
-              {variant !== "landing" && <OldEffect />}
+    <main className='h-screen '>
               <ContentBox variant={variant}>{children}</ContentBox>
-            </div>
-          </div>
-        </div>
-      </div>
+    
     </main>
   );
 };
