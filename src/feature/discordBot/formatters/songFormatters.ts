342e4c5802a9525f6a4841b0e900d072d128638f
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2025-12-23 22:20:15 +0100

    feat: Add Discord bot formatters for song and general activity logs, and update the project URL in the README.

--- src/feature/discordBot/formatters/songFormatters.ts
+++ src/feature/discordBot/formatters/songFormatters.ts
@@ -16,7 +16,7 @@ export class LearnedSongFormatter implements SongFormatter {
       embeds: [
         {
           title: "Utwór Opanowany",
-          description: `**[${displayName}](<https://www.cwiczymy-razem.pl//user/${log.uid}> " ")** nauczył się utworu **${log.songArtist} ${log.songTitle}**`,
+          description: `**[${displayName}](<https://riff.quest/user/${log.uid}> " ")** nauczył się utworu **${log.songArtist} ${log.songTitle}**`,
           color: 0x2ecc71,
         },
       ],
@@ -31,7 +31,7 @@ export class WantToLearnSongFormatter implements SongFormatter {
       embeds: [
         {
           title: "Nauka Utworu",
-          description: `**[${displayName}](<https://www.cwiczymy-razem.pl//user/${log.uid}> " ")** chce nauczyć się utworu **${log.songArtist} ${log.songTitle}**`,
+          description: `**[${displayName}](<https://riff.quest/user/${log.uid}> " ")** chce nauczyć się utworu **${log.songArtist} ${log.songTitle}**`,
           color: 0xf1c40f,
         },
       ],
@@ -46,7 +46,7 @@ export class LearningSongFormatter implements SongFormatter {
       embeds: [
         {
           title: "Nauka Utworu",
-          description: `**[${displayName}](<https://www.cwiczymy-razem.pl//user/${log.uid}> " ")** uczy się utworu **${log.songArtist} ${log.songTitle}**`,
+          description: `**[${displayName}](<https://riff.quest/user/${log.uid}> " ")** uczy się utworu **${log.songArtist} ${log.songTitle}**`,
           color: 0xe67e22,
         },
       ],
@@ -61,7 +61,7 @@ export class AddedSongFormatter implements SongFormatter {
       embeds: [
         {
           title: "Nowy Utwór Dodany",
-          description: `**[${displayName}](<https://www.cwiczymy-razem.pl//user/${log.uid}> " ")** dodał utwór **${log.songArtist} ${log.songTitle}**`,
+          description: `**[${displayName}](<https://riff.quest/user/${log.uid}> " ")** dodał utwór **${log.songArtist} ${log.songTitle}**`,
           color: 0x3498db,
         },
       ],
@@ -76,14 +76,13 @@ export class DifficultyRateSongFormatter implements SongFormatter {
       embeds: [
         {
           title: "Ocena Trudności Utworu",
-          description: `**[${displayName}](<https://www.cwiczymy-razem.pl//user/${log.uid}> " ")** ocenił trudność utworu **${log.songArtist} ${log.songTitle}**`,
+          description: `**[${displayName}](<https://riff.quest/user/${log.uid}> " ")** ocenił trudność utworu **${log.songArtist} ${log.songTitle}**`,
           color: 0x9b59b6,
           fields: [
             {
               name: "Ocena ",
-              value: `${getStars(log.difficulty_rate ?? 0)} (${
-                log.difficulty_rate
-              }/10)`,
+              value: `${getStars(log.difficulty_rate ?? 0)} (${log.difficulty_rate
+                }/10)`,
               inline: false,
             },
           ],
