cbcab540723cbd8a2c4c1703892457b5abd4a2c7
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2026-01-04 20:47:07 +0100

    feat: Implement a new achievements module with display components, utilities, and comprehensive documentation.

---
+++ src/feature/achievements/index.ts
@@ -0,0 +1,6 @@
+export { AchievementCard } from "./components/Card/AchievementCard";
+export { default as AchievementIcon } from "./components/AchievementIcon";
+export { AchievementManager } from "./utils/AchievementsManager";
+export { achievementsData, achievementsMap, achievementsCounts } from "./data/achievementsData";
+export type { AchievementsRarityType } from "./data/achievementsRarity";
+export * from "./types";
