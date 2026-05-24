3654efe24b6414cc4b1193219ffd246e71060380
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2026-05-24 15:30:41 +0200

    off sentry

--- next.config.js
+++ next.config.js
@@ -130,11 +130,8 @@ module.exports = withSentryConfig(module.exports, {
   // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
 
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  tunnelRoute: "/monitoring",
+  // tunnelRoute disabled to reduce Vercel edge requests
+  // tunnelRoute: "/monitoring",
 
   webpack: {
     // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
