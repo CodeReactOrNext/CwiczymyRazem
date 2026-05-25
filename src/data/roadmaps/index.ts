00bfb43d678d7dcebef47c094405cf85a3e0a665
Michaljapko <88621322+Michaljapko@users.noreply.github.com>
2026-05-25 19:58:23 +0200

    new roadmap

--- src/data/roadmaps/index.ts
+++ src/data/roadmaps/index.ts
@@ -2,6 +2,7 @@ import type { StaticRoadmap } from "feature/aiCoach/types/roadmap.types";
 
 import guitarImprov from "./guitar-improvisation-fundamentals.json";
 import johnMayer from "./i-want-to-play-in-the-style-of-john-maye.json";
+import martyFriedman from "./i-want-to-play-in-the-style-of-marty-fri.json";
 import hendrix from "./play-in-the-style-of-jimi-hendrix.json";
 import petrucci from "./play-in-the-style-of-john-petrucci.json";
 import rhythmBasics from "./rhythm-guitar-basics.json";
@@ -10,6 +11,7 @@ const roadmaps: StaticRoadmap[] = [
   johnMayer as StaticRoadmap,
   { ...hendrix, image: "/images/roadmap/hendrix.png" } as StaticRoadmap,
   { ...petrucci, image: "/images/roadmap/petrucci.png" } as StaticRoadmap,
+  { ...martyFriedman, image: "/images/roadmap/friedman.png" } as StaticRoadmap,
   { ...guitarImprov, image: "/images/roadmap/impro.png" } as StaticRoadmap,
   { ...rhythmBasics, image: "/images/roadmap/rythm.png" } as StaticRoadmap,
 ];
